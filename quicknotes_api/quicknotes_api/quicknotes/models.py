f25f7cbd1f0fccd496662c4aefad9cd668b89cb0
Tony Blondeau NYA <tonybnya@gmail.com>
2025-12-07 11:22:06 +0100

    feat: add model for Note

--- quicknotes_api/quicknotes_api/quicknotes/models.py
+++ quicknotes_api/quicknotes_api/quicknotes/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+    def __repr__(self) -> str:
+        return self.__str__()
