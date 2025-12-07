0e7f48525fefdcedf6077046da8e8e49c2cab987
Tony Blondeau NYA <tonybnya@gmail.com>
2025-12-07 14:28:55 +0100

    feat: embedded the resource into an object for the response body

--- quicknotes_api/quicknotes/views.py
+++ quicknotes_api/quicknotes/views.py
@@ -6,6 +6,7 @@ Author      : @tonybnya
 
 from django.http import HttpResponse, JsonResponse # type: ignore
 from rest_framework.viewsets import ModelViewSet # type: ignore
+from rest_framework.response import Response # type: ignore
 from quicknotes.serializers import NoteSerializer
 from .models import Note
 
@@ -17,3 +18,13 @@ def home(request):
 class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    def list(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'data': serializer.data})
+
+    def retrieve(self, request):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({'data': serializer.data})
