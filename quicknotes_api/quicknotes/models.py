2e232dd20c0b4a97a5c2c194b2c77865fbc37302
Tony Blondeau NYA <tonybnya@gmail.com>
2025-12-08 20:01:41 +0100

    feat: create model Collection

--- quicknotes_api/quicknotes/models.py
+++ quicknotes_api/quicknotes/models.py
@@ -7,9 +7,21 @@ Author      : @tonybnya
 from django.db import models # type: ignore
 
 
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
 class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name="notes", null=True)
 
     def __str__(self) -> str:
         return self.title
