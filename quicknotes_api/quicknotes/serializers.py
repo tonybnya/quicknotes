25ef0d221064af4c76a86066846f3198a3aefc0e
Tony Blondeau NYA <tonybnya@gmail.com>
2025-12-08 19:37:20 +0100

    fix+feat: update the serializer of the Note model to automaticallt grap all the fields

--- quicknotes_api/quicknotes/serializers.py
+++ quicknotes_api/quicknotes/serializers.py
@@ -11,4 +11,4 @@ from quicknotes.models import Note
 class NoteSerializer(ModelSerializer):
     class Meta:
         model = Note
-        fields = ['id', 'title', 'content']
+        fields = "__all__"
