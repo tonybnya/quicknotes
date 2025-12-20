f53be252fd3cbfacfccec70a644c6c181f6e1d6a
Tony Blondeau NYA <tonybnya@gmail.com>
2025-12-20 12:34:10 +0100

    build: init the client app and start implementing the landing page

---
+++ quicknotes_client/src/App.tsx
@@ -0,0 +1,13 @@
+// import { Button } from "@/components/ui/button";
+import Hero from "@/components/kokonutui/background-paths";
+
+const App = () => {
+  return (
+    <Hero />
+    // <div className="flex min-h-svh flex-col items-center justify-center">
+    //   <Button>Click me</Button>
+    // </div>
+  )
+}
+
+export default App;
