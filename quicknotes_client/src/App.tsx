374ac10773560eacb4e3160d91083cc628b4e18f
Tony Blondeau NYA <tonybnya@gmail.com>
2025-12-23 22:31:22 +0100

    feat: add auth routes

--- quicknotes_client/src/App.tsx
+++ quicknotes_client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import Landing from "@/pages/Landing";
 import Quicknotes from "@/pages/Quicknotes";
+import AuthPage from "@/pages/AuthPage";
 
 import NotFoundPage from "@/pages/NotFoundPage";
 
@@ -11,13 +12,28 @@ const App = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Layout />}>
+            {/* <Route path="/" element={<Layout />}>
             <Route index element={<Landing />} />
             <Route path="accueil" element={<Landing />} />
             <Route path="quicknotes" element={<Quicknotes />} />
             </Route>
 
-            <Route path="*" element={<NotFoundPage />} />
+            <Route path="*" element={<NotFoundPage />} /> */}
+
+          {/* Main Website Routes */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Landing />} />
+            <Route path="accueil" element={<Landing />} />
+            <Route path="login" element={<AuthPage />} />
+            <Route path="register" element={<AuthPage />} />
+          </Route>
+
+          {/* Dashboard/App Routes */}
+          {/* We use a separate route or a flag for Quicknotes if you want to hide the landing footer */}
+          <Route path="/quicknotes" element={<Quicknotes />} />
+
+          {/* 404 Route */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
     </BrowserRouter>
   );
